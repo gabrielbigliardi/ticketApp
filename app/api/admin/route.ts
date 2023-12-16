95c02b1e46004bb4e6fa3d86aff6d2db8bfbdb9e
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-12-16 19:40:33 -0300

    trying to connet api

---
+++ app/api/admin/route.ts
@@ -0,0 +1,55 @@
+import Ticket from "@/app/(models)/Ticket";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { NextResponse } from "next/server";
+
+// type TicketData = {
+//     title: String,
+//     description: String,
+//     category: String,
+//     priority: Number,
+//     progress: Number,
+//     status: String,
+//     active: Boolean
+// }
+
+
+
+
+export default async function POST(req: any) {
+    console.log("RANDOM")
+    try {
+        const body = await req.json()
+        const ticketData = body.formData
+        await Ticket.create(ticketData)
+        return NextResponse.json({ message: "Ticket Created" }, { status: 201 })
+    } catch (err) {
+        return NextResponse.json({ message: "Error", err }, { status: 500 })
+    }
+
+
+}
+
+
+
+
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     console.log("HANDLER")
+//     if (req.method === 'POST') {
+//         try {
+
+//             const data: TicketData = req.body;
+//             console.log(data)
+
+//             // Faça o que precisa com os dados (por exemplo, salvar no banco de dados)
+
+//             res.status(200).json({ success: true, message: 'Ticket criado com sucesso' });
+//         } catch (error) {
+//             console.error('Erro ao processar a solicitação:', error);
+//             res.status(500).json({ success: false, message: 'Erro interno do servidor' });
+//         }
+//     } else {
+//         res.status(405).json({ success: false, message: 'Método não permitido' });
+//     }
+// }
+
